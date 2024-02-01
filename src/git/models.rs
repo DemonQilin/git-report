c3a13555ce537d24d39b61a713d4904eb425e155
Demonqilin <velezloaizaesteban9012015@gmail.com>
2024-02-01 07:44:12 -0500

    chore(Git): Implement Deserialization to Commit model

--- src/git/models.rs
+++ src/git/models.rs
@@ -1,9 +1,13 @@
+use chrono::serde::ts_seconds;
 use chrono::{DateTime, Utc};
+use serde::Deserialize;
 
-#[derive(Debug)]
+#[derive(Deserialize, Debug)]
 pub struct Commit {
     pub message: String,
     pub author: String,
+    #[serde(with = "ts_seconds")]
     pub time: DateTime<Utc>,
+    #[serde(default)]
     pub changes: Vec<String>,
 }
